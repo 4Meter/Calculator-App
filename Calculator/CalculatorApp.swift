d0fb27eb329bf7d47c070ef28d0371b8df0fadf6
Yen Hung <yen4meter@gmail.com>
2022-03-27 15:15:54 +0800

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by user on 2022/3/27.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
